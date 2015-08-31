982c460a42b796b6b6055e55f17fa07f0aca4eb2
João Pinto <lamego.pinto@gmail.com>
2015-08-31 21:32:55 +0100

    Replaced custom max tracking with proper event list creation

--- verbosegc_stats.py
+++ verbosegc_stats.py
@@ -18,33 +18,25 @@ import sys
 import re
 from fileinput import FileInput, hook_compressed
 from dateutil.parser import parse
+from os.path import dirname
+from optparse import OptionParser
 
-class GC_Stats:
+class GC_Event:
 
-    def __init__(self):
-        self.last_ts = None
-        self.first_ts = None
-        self.first_date = None
-        self.last_date = None
-        self.heap_after_max = 0
-        self.heap_after_max_per = 0
-        self.heap_after_max_ds = ''
-        self.pause_time_max = 0
-        self.pause_time_max_ds = ''
-        self.pause_times = []
-        self.heap_before = []
-        self.heap_after = []
-        self.interval_times = []
-
+    def __init__(self, date_stamp, time_stamp, pause_time, interval, heap):
+        self.date_stamp = date_stamp
+        self.time_stamp = time_stamp
+        self.pause_time = pause_time
+        self.interval = interval
+        self.heap_before, self.heap_after, self.heap_size =  heap
+        self.permgen_before = self.permgen_after = self.permgen_size = 0
 
 class VerboseGCParser:
 
     def __init__(self, filename, jvm_name):
         self.filename = filename
         self.jvm_name = jvm_name
-        self.GC = GC_Stats()
-        self.fullGC = GC_Stats()
-        self.allGC = GC_Stats()
+        self.gc_events =  {'GC': [], 'FullGC': []}
 
     def parse(self):
         for line in FileInput(files=[self.filename], openhook=hook_compressed):
@@ -93,85 +85,105 @@ class VerboseGCParser:
         parsed_line = re.findall(r'(\S+):\s+(\S+):\s+(.*)', line)
         if not parsed_line:
             raise Exception('Unknown line format: '+parsed_line)
+        ev = None
         date_stamp, time_stamp, gc_info = parsed_line[0]
         time_stamp = float(time_stamp)
-        date_stamp = parse(date_stamp).strftime('%d-%b-%Y %H:%M:%S')
-        for level, content in self.bracket_contents(gc_info):
+        date_stamp = parse(date_stamp).strftime('%d/%b %H:%M:%S')
+        for level, content in sorted(self.bracket_contents(gc_info)):
             if level == 0:
                 content_type = content.split('[')[0].strip()
-                gc_type = None
                 if content_type == 'GC':
-                    gc_type = self.GC
+                    gc_event_list =  self.gc_events['GC']
                 elif content_type == 'Full GC':
-                    gc_type = self.fullGC
-                if gc_type:
+                    gc_event_list =  self.gc_events['FullGC']
+                else:
+                    gc_event_list = None
+                if gc_event_list is not None:
                     inside_content = self.bracket_inside(content)
                     heaps = re.findall(COLLECT_REGEX, inside_content)[0]
-                    heap_before, heap_after, heap_size = map(lambda x: int(x), heaps)
-                    for stats_gc_type in [gc_type, self.allGC]:
-                        if stats_gc_type.last_ts is not None:
-                            interval = time_stamp - stats_gc_type.last_ts
-                            stats_gc_type.interval_times.append(interval)
-                        else:
-                            stats_gc_type.first_ts = time_stamp
-                            stats_gc_type.first_date = date_stamp
-                        pause_time = float(re.findall(r'(\S*) secs', inside_content)[0])
-                        stats_gc_type.pause_times.append(pause_time)
-
-                        stats_gc_type.last_ts = time_stamp
-                        stats_gc_type.last_date = date_stamp
-
-                        # Update maxs if needed
-                        if heap_after> stats_gc_type.heap_after_max:
-                            stats_gc_type.heap_after_max = heap_after
-                            stats_gc_type.heap_after_max_per = float(heap_after) / (heap_size) * 100
-                            stats_gc_type.heap_after_max_ds = date_stamp
-                        if pause_time > stats_gc_type.pause_time_max:
-                            stats_gc_type.pause_time_max = pause_time
-                            stats_gc_type.pause_time_max_ds = date_stamp
-            elif level == 1:
+                    heap = map(lambda x: int(x), heaps)
+                    interval = 0
+                    if len(gc_event_list) > 0:
+                        interval = time_stamp - gc_event_list[-1].time_stamp
+                    pause_time = float(re.findall(r'(\S*) secs', inside_content)[0])
+                    ev = GC_Event(date_stamp, time_stamp, pause_time, interval, heap)
+                    gc_event_list.append(ev)
+            elif ev and level == 1:
                 area_type, collection_info = content.split()
                 inside_content = self.bracket_inside(content)
                 if area_type == 'PSPermGen:':
                     heaps = re.findall(COLLECT_REGEX, inside_content)[0]
-                    heap_before, heap_after, heap_size = map(lambda x: int(x), heaps)
-                    self.allGC.permgen_max_per = float(heap_after) / (heap_size) * 100
+                    heaps = map(lambda x: int(x), heaps)
+                    ev.permgen_before, ev.permgen_after, ev.permgen_size = heaps
 
 
     def print_summary(self):
-        total_interval_period = sum(self.allGC.interval_times)
+        gc_events = self.gc_events['GC']
+        fullgc_events = self.gc_events['FullGC']
+        all_events = gc_events + fullgc_events
+
         report = ""
         report += "*** Analyzed GC activity for %s from %s to %s (%s)\n" % (
             self.jvm_name,
-            self.allGC.first_date,
-            self.allGC.last_date,
-            self.seconds2hours(total_interval_period)
+            all_events[0].date_stamp,
+            all_events[-1].date_stamp,
+            self.seconds2hours(max([x.time_stamp for x in all_events]) - min([x.time_stamp for x in all_events]))
         )
-        report += """The max heap used memory (after a full GC) was %dK [%2.2f%%] on %s.\n"""\
-            % (self.fullGC.heap_after_max, self.fullGC.heap_after_max_per, self.fullGC.heap_after_max_ds)
-        if 'permgen_max_per' in self.allGC.__dict__:
-            report += """The max PSPermGen memory usage was %2.2f%%\n"""\
-                % (self.allGC.permgen_max_per)
+        max_heap_after = max([x.heap_after for x in fullgc_events])
+        max_heap_after_perc = max([float(x.heap_after) / x.heap_size * 100  for x in fullgc_events])
+        mas_heap_time_stamp = filter(lambda x: x.heap_after == max_heap_after, fullgc_events)[0].date_stamp
+        report += """The max heap used memory (after a full GC) was %dK [%2.2f%%] on %s.\n""" \
+            % (max_heap_after, max_heap_after_perc, mas_heap_time_stamp)
+        max_permgen_after = max([x.permgen_after for x in all_events])
+        max_permgen_after_perc = max([float(x.permgen_after) / x.permgen_size * 100  for x in all_events if x.permgen_size] or [0])
+        max_permgen_after_date_stamp = filter(lambda x: x.permgen_after == max_permgen_after, all_events)[0].date_stamp
+        report += """The max PSPermGen was %dK [%2.2f%%] on %s.\n""" \
+            % (max_permgen_after, max_permgen_after_perc, max_permgen_after_date_stamp)
+        max_pause_time = max([x.pause_time for x in all_events])
+        max_pause_time_stamp = filter(lambda x: x.pause_time == max_pause_time, all_events)[0].date_stamp
+        report += """The max pause time was %2.1fs on %s.\n""" \
+            % (max_pause_time, max_pause_time_stamp)
 
-        report += """The max pause time was %2.1fs on %s.\n"""\
-            % (self.allGC.pause_time_max, self.allGC.pause_time_max_ds)
-        report += """Performed %d full GCs, with a pause time of %.2fs (avg) at %s (avg) intervals.\n""" % (\
-            len(self.fullGC.pause_times),
-            float(sum(self.fullGC.pause_times))/len(self.fullGC.pause_times),
-            self.seconds2hours(float(sum(self.fullGC.interval_times))/len(self.fullGC.interval_times))
-            )
-        report += """Performed %d minor GCs, with a pause time of %.2fs (avg) at %s (avg) intervals.\n""" % (\
-            len(self.GC.pause_times),
-            float(sum(self.GC.pause_times))/len(self.GC.pause_times),
-            self.seconds2hours(float(sum(self.GC.interval_times))/len(self.GC.interval_times))
-            )
-        report += "***\n"
+        for key, keystr  in [('FullGC', 'full GCs'), ('GC', 'minor GCs')]:
+            gc_events = self.gc_events[key]
+            count = len(gc_events)
+            pause_avg = sum([x.pause_time for x in gc_events]) / float(count)
+            interval_avg = sum([x.interval for x in gc_events]) / float(count-1)
+            heap_after_avg = sum([x.heap_after for x in gc_events]) / float(count)
+            report += """%d %s, with a pause time of %.2fs (avg) at %s (avg) intervals, resulting in %.0fK (avg) used heap.\n""" \
+                % (
+                    count, keystr,
+                    pause_avg,
+                    self.seconds2hours(interval_avg),
+                    heap_after_avg,
+                )
+
 
         print report
 
-gclog_name = sys.argv[1]
-jvm_name = sys.argv[2] if len(sys.argv)>2 else 'JVM_NAME'
+def parse_args():
+    cmd_parser = OptionParser()
 
-parser = VerboseGCParser(gclog_name, jvm_name)
-parser.parse()
-parser.print_summary()
+    cmd_parser.add_option("-q", "--quiet", dest="quiet",
+        help="No information ouput", action="store_true", default=False)
+    cmd_parser.add_option("-r", "--results", dest="results",
+        help="Calculate perofrmance results", action="store_true", default=False)
+    (options, args) = cmd_parser.parse_args()
+
+    return options, args
+
+def main():
+    (options, args) = parse_args()
+    gclog_name = args.pop(0)
+    jvm_name = args.pop(0) if args else dirname(gclog_name).split('/')[-1]
+    parser = VerboseGCParser(gclog_name, jvm_name)
+    parser.parse()
+    parser.print_summary()
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Interrupted"
